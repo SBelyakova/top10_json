98e6068c99ee619853f5c0119369f0c41e54c765
SBelyakova <wild_cherry@mail.ru>
2020-10-23 18:12:11 +0000

    add (json, xml)

--- main.py
+++ main.py
@@ -1,44 +1,36 @@
-def count_top10_json():
-  import json
-  with open("files/newsafr.json") as f:
-    json_data = json.load(f)
+import json
+import xml.etree.ElementTree as ET
+from collections import Counter
 
-  titles = json_data["rss"]["channel"]["items"]
-  
-  desc = []
-  unique_word = []
-  
-  for title in titles:
-    # print(title[0])
-    desc.append(title['description'])
-  # print(desc[1])
-  # print()
-  for string in desc:
-    uniques = string.split(' ')
-    # print(uniques)
-    for word in uniques:
-      if len(word) > 6:
-        unique_word.append(word)      
-  # print(unique_word)
-  counts = []
 
-  for words in unique_word:
-    count = 0
-    for word in unique_word:
-      if word == words:
-        count += 1
-    # print(count, words)
-    if (count, words) not in counts:
-      counts.append((count, words))
-  # print(counts)
-  counts.sort()
-  # print(counts)
-  counts.reverse()
-  # print(counts[0:10])
+def get_text_json():
+  with open('files/newsafr.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
 
-  for i in range (0, 10):
-    count, word = counts[i]
-    print(f'на {i+1} месте: слово \"{word}\", встречается {count} раз(а)')
-  return
-  
-count_top10_json()
+  news_list =[title['description'] for title in data["rss"]["channel"]["items"]]
+  return news_list
+
+def get_text_xml():
+  parser = ET.XMLParser(encoding="utf-8")
+  tree = ET.parse("files/newsafr.xml", parser)
+  root = tree.getroot()
+  news_list = [item.find("description").text for item in root.findall("channel/item")]
+  return news_list
+
+
+def get_top_words(news_list):
+  news_list = list(map(lambda x: x.split(' '), news_list))
+  news_words = []
+  for news in news_list:
+    more_than_six = list(filter(lambda x: len(x) > 6, news))
+    news_words.extend(more_than_six)
+
+  news_words = Counter(news_words)
+  news_words = sorted(news_words.items(), key=lambda x: x[1], reverse=True)
+  result = [news_words[i] for i in range(0, 10)]
+  return result
+
+if __name__ == "__main__":
+  print(get_top_words(get_text_json()), sep='\n')
+  print()
+  print(get_top_words(get_text_xml()), sep='\n')
