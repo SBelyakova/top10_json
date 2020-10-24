e5c070067efb402b6c4e1064cac08c054da53324
SBelyakova <wild_cherry@mail.ru>
2020-10-24 15:46:59 +0000

    fixed

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import xml.etree.ElementTree as ET
 from collections import Counter
 
 
-def get_text_json():
-  with open('files/newsafr.json', 'r', encoding='utf-8') as f:
+def get_text_json(json_file):
+  with open(json_file, 'r', encoding='utf-8') as f:
     data = json.load(f)
 
   news_list =[title['description'] for title in data["rss"]["channel"]["items"]]
   return news_list
 
-def get_text_xml():
+def get_text_xml(xml_file):
   parser = ET.XMLParser(encoding="utf-8")
-  tree = ET.parse("files/newsafr.xml", parser)
+  tree = ET.parse(xml_file, parser)
   root = tree.getroot()
   news_list = [item.find("description").text for item in root.findall("channel/item")]
   return news_list
@@ -26,11 +26,10 @@ def get_top_words(news_list):
     news_words.extend(more_than_six)
 
   news_words = Counter(news_words)
-  news_words = sorted(news_words.items(), key=lambda x: x[1], reverse=True)
-  result = [news_words[i] for i in range(0, 10)]
+  result = [news_words.most_common()[i] for i in range(0, 10)]
   return result
 
 if __name__ == "__main__":
-  print(get_top_words(get_text_json()), sep='\n')
+  print(get_top_words(get_text_json('files/newsafr.json')), sep='\n')
   print()
-  print(get_top_words(get_text_xml()), sep='\n')
+  print(get_top_words(get_text_xml("files/newsafr.xml")), sep='\n')
